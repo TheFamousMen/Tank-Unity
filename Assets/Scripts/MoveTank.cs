5c563e8a1d5bd9b4c25cf8ff6b5f6001c1b7c9f3
Natan143 <Natanek@MacBook-Pro-Natanek.local>
2015-01-23 21:29:07 +0100

    New Files

---
+++ Assets/Scripts/MoveTank.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTank : MonoBehaviour {
+	public float rotationSpeed = 5f;
+	public float moveSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+		{	
+			transform.Translate(Vector3.forward * moveSpeed);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{	
+			transform.Translate(-Vector3.forward * moveSpeed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{	
+			transform.Rotate(-Vector3.up * rotationSpeed);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{	
+			transform.Rotate(Vector3.up * rotationSpeed);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/RotationTurret.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationTurret : MonoBehaviour {
+
+	public Camera camera;
+	private Transform[] transforms;
+	// Use this for initialization
+	void Start () {
+		transforms = gameObject.GetComponentsInChildren<Transform> ();
+		foreach (Transform t in transforms) {
+				
+			if (t.gameObject.name == "turret")
+			{
+				turret = t;
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition; //(x,y,z)
+		mousePos.z = camera.transform.position.y;
+
+		Vector3 worldPos camera.ScreenToWorldPoint (mousePos);
+		turret.LookAt(worldPos);
+
+
+
+	}
+}
